73eda3c0d03945607edc6248c608d4441d0e25df
Alim Abdul Jabbar <hello.liimalim@gmail.com>
2021-08-01 06:39:35 +0700

    started

---
+++ application/views/home.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <script src="<?php echo base_url('assets/js/jquery-3.2.1.slim.min.js') ?>"></script>
+    <script src="<?php echo base_url('assets/js/popper.min.js') ?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+  </body>
+</html>

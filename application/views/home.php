13e4bffd9dd75b7edb4a82573b8af5e5860c2308
Alim Abdul J <alimabduljabbar1746@gmail.com>
2021-08-12 10:30:25 +0700

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -8,7 +8,7 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <script src="<?php echo base_url('assets/js/jquery-3.2.1.slim.min.js') ?>"></script>
+    <script src="<?php echo base_url('assets/js/jquery-3.2.1.min.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/popper.min.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
   </body>
